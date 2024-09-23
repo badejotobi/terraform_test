dc9c0fe42294b8059a330bbce5e0fd4e6ab8443f
badejotobi <133620914+badejotobi@users.noreply.github.com>
2024-09-23 11:59:32 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,44 +37,6 @@ resource "aws_s3_bucket" "terraform_state" {
   }
 }
 
-
-resource "aws_s3_bucket_ownership_controls" "example" {
-  bucket = aws_s3_bucket.terraform_state.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
-resource "aws_s3_bucket_acl" "example" {
-  depends_on = [aws_s3_bucket_ownership_controls.example]
-
-  bucket = aws_s3_bucket.terraform_state.id
-  acl    = "private"
-}
-resource "aws_kms_key" "mykey" {
-  description             = "This key is used to encrypt bucket objects"
-  deletion_window_in_days = 10
-}
-
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
-  bucket = aws_s3_bucket.terraform_state.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      kms_master_key_id = aws_kms_key.mykey.arn
-      sse_algorithm     = "aws:kms"
-    }
-  }
-}
-resource "aws_s3_bucket_versioning" "versioning_example" {
-  bucket = aws_s3_bucket.terraform_state.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-
 resource "aws_dynamodb_table" "terraform_locks" {
   name         = "terraform-lock-table"
   billing_mode = "PAY_PER_REQUEST" # No need to manage capacity
